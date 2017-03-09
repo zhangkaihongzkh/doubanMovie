a37753844ffd5f96c71068ef6270a17df58cd1a0
zhangkaihongzkh <953628520@qq.com>
2017-03-09 11:34:44 +0800

    增加了查看电影人信息模块

--- app/app.js
+++ app/app.js
@@ -2,78 +2,82 @@
 
 // Declare app level module which depends on views, and components
 angular.module('movieCat', [
-  'ngRoute',
-  //'movieCat.in_theaters',
-  'movieCat.movie_detail',
-  'movieCat.movie_list',
+        'ngRoute',
+        //'movieCat.in_theaters',
+        'movieCat.movie_detail',
+        'movieCat.person_detail',
+        'movieCat.movie_list',
 
-  'movieCat.directive.auto_focus',//自动监听导航栏变化
+        'movieCat.directive.auto_focus', //自动监听导航栏变化
 
-])
-//公用配置项
-.constant('appConfig',{
-	pageSize: 10,
-	listApiAdress:'https://api.douban.com/v2/movie/',
-	detailApiAdress:'https://api.douban.com/v2/movie/subject/',
-	//      /v2/movie/subject/:id
-	//		/v2/movie/in_theaters/1
-})
-.config(['$routeProvider', function($routeProvider) {
-	//初始化跳转到第一个页面
-  $routeProvider.otherwise({redirectTo: '/in_theaters/1'});
-}])
+    ])
+    //公用配置项
+    .constant('appConfig', {
+        pageSize: 10,
+
+        listApiAdress: 'https://api.douban.com/v2/movie/', //列表api
+        detailApiAdress: 'https://api.douban.com/v2/movie/subject/', //电影详情api
+        personApiAdress: 'https://api.douban.com/v2/movie/celebrity/', //人物信息api
+        //      /v2/movie/subject/:id
+        //		/v2/movie/in_theaters/1
+    })
+    .config(['$routeProvider', function($routeProvider) {
+        //初始化跳转到第一个页面
+        $routeProvider.otherwise({ redirectTo: '/in_theaters/1' });
+    }])
 
 
 //导航栏
 .controller('Nav', [
-	'$scope',
-	'$location',
-	function($scope,$location){//导航栏
-		$scope.$location = $location;
-		$scope.$watch('$location.path()', function(newValue) {
-			if(newValue.startsWith('/in_theaters')){
-				$scope.type = 'in_theaters';
-			} else if(newValue.startsWith('/coming_soon')){
-				$scope.type = 'coming_soon';
-			}else if(newValue.startsWith('/top250')){
-				$scope.type = 'top250';
-			}else if(newValue.startsWith('/search')){//搜索结果
-				$scope.type = 'search';
-			}
-		});
-	}
+    '$scope',
+    '$location',
+    function($scope, $location) { //导航栏
+        $scope.$location = $location;
+        $scope.$watch('$location.path()', function(newValue) {
+            if (newValue.startsWith('/in_theaters')) {
+                $scope.type = 'in_theaters';
+            } else if (newValue.startsWith('/coming_soon')) {
+                $scope.type = 'coming_soon';
+            } else if (newValue.startsWith('/top250')) {
+                $scope.type = 'top250';
+            } else if (newValue.startsWith('/search')) { //搜索结果
+                $scope.type = 'search';
+            }
+        });
+    }
 ])
 
 //搜索模块
 .controller('SearchFunc', [
-	'$scope',
-	'$route',
-	'$routeParams',
-	'$location',
-	function($scope,$route,$routeParams,$location){
-		$scope.input = '';//搜索内容
-		$scope.$location = $location;//用于监听地址变化
-		//搜索功能
-		$scope.search = function(){
-			if(!$routeParams.category){//当前在详情页面
-				//console.log(1111);
-				//$location.path('/detail/').replace('/search/');
-				var tmpUrl = '/search/1?q='+$scope.input;
-				$location.url(tmpUrl);
-				//$route.updateParams({category:'search',q:$scope.input});
-				/*console.log($location.path());
-				console.log($scope.input);
-				console.log(tmpUrl);*/
-				//$route.updateParams({q:$scope.input});
-				//$route.updateParams({q:$scope.input});
-				//http://localhost:8090/doubanMovie/app/index.html#/search/1?q=aaa
+    '$scope',
+    '$route',
+    '$routeParams',
+    '$location',
+    function($scope, $route, $routeParams, $location) {
+        $scope.input = ''; //搜索内容
+        $scope.$location = $location; //用于监听地址变化
+        //搜索功能
+        $scope.search = function() {
+            if (!$routeParams.category) { //当前在详情页面
+                //console.log(1111);
+                //$location.path('/detail/').replace('/search/');
+                var tmpUrl = '/search/1?q=' + $scope.input;
+                $location.url(tmpUrl);
+                //$route.updateParams({category:'search',q:$scope.input});
+                /*console.log($location.path());
+                console.log($scope.input);
+                console.log(tmpUrl);*/
+                //$route.updateParams({q:$scope.input});
+                //$route.updateParams({q:$scope.input});
+                //http://localhost:8090/doubanMovie/app/index.html#/search/1?q=aaa
 
-			}else{//当前在列表页面
-				$route.updateParams({category:'search',q:$scope.input});
+            } else { //当前在列表页面
+                $route.updateParams({ category: 'search', q: $scope.input });
 
-			}
+            }
 
-			//$scope.input = $routeParams.q;
-			//console.log($scope.input);
-	}
-}])
+            //$scope.input = $routeParams.q;
+            //console.log($scope.input);
+        }
+    }
+])
---
+++ app/person_detail/controller.js
@@ -0,0 +1,46 @@
+(function(angular){
+
+	'use strict';
+
+	var module = angular.module('movieCat.person_detail', [
+		'ngRoute',
+		'movieCat.services.http',
+		]);
+
+	//配置路由
+	module.config(['$routeProvider', function($routeProvider) {
+	  $routeProvider.when('/person/:id', {
+	    templateUrl: 'person_detail/view.html',
+	    controller: 'PersonDetailController'
+	  });
+	}]);
+
+
+
+
+	module.controller('PersonDetailController', [
+		'$scope',
+		'$route',		//路由模块
+		'$routeParams',//路由参数
+		'HttpService',
+		'appConfig',
+		function($scope,$route,$routeParams,HttpService,appConfig) {
+			$scope.personDetail = {title:'loading...'}; //电影的详细参数
+			//$scope.personDetail.title = 'loading...';
+			//加载是否完成 true表示正在加载
+			$scope.loading = true;
+
+			var doubanAPI = appConfig.personApiAdress + $routeParams.id;
+
+			HttpService.jsonp(doubanAPI,function(data){
+				$scope.personDetail = data;
+				$scope.loading = false;
+				console.log(data);
+				$scope.$apply();//重新渲染页面
+
+			});
+		}
+	]);
+
+})(angular);
+
